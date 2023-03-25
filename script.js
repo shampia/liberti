d00084fd2671d83bcae570d4f6894e5b339f104d
JustLouie <giorgi.shalamberidze2@gmail.com>
2023-03-25 14:27:22 +0400

    Liberti app updated

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-
-
-
-
 const itemButtons = document.querySelectorAll('section.item .item-btn button')
 const items = document.querySelectorAll('section.item .items .items-item')
 
 const visibleAll = () => {
         items.forEach(item => (
-        item.style.display = 'block'
+        item.style.display = 'flex'
     ))
 }
 
